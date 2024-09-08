69d1917edde6915a154e9f3e4a57beb1da36e0c4
naagaraa <ekabersinar@gmail.com>
2024-09-08 20:35:39 +0700

    fix backend api

--- api/routes/api.go
+++ api/routes/api.go
@@ -1,10 +1,20 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 // Exported function Main (needs to be uppercase to be exported)
 func Routes() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,PUT,DELETE",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
@@ -14,6 +24,11 @@ func Routes() {
 	})
 	app.Get("/about", func(c *fiber.Ctx) error {
 		return c.SendString("about pages")
+	})
+
+	// Define your routes
+	app.Get("/api/v1/hello", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "Hello from Go Fiber!"})
 	})
 
 	app.Listen(":3000")
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useEffect, useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,41 +9,31 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [message, setMessage] = useState("");
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    fetch(`${apiUrl}/api/v1/hello`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors", // Ensure CORS mode is enabled
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching API:", error));
+  }, [apiUrl]);
+
   return (
     <div className="font-sans p-4">
       <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <div>{message}</div>
     </div>
   );
 }
