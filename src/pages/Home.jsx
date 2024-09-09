d8e5977bc1ba227044cf7cbefe5537c8f895f491
naagaraa <ekabersinar@gmail.com>
2024-09-09 15:03:16 +0700

    update react

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { Outlet, Link } from 'react-router-dom';
+
 function Home() {
   return (
     <>
       <p>home</p>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Outlet />
     </>
   )
 }
